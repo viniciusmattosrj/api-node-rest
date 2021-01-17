262dcfe00e548dbc488ba1d58c55f5ed259d1c83
Vinicius Mattos <mvinicius@funcionalcorp.com.br>
2021-01-17 16:19:20 -0300

    17012021 - Adicionando dotEnv e normalizePort.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config()
+const normalizePort = require('normalize-port');
 
 const app = express();
+const port = normalizePort(process.env.PORT || '3000');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-require('./controllers/authController')(app);
-require('./controllers/projectController')(app);
+require('./app/controllers/index')(app);
 
-app.listen(3000);
+app.listen(port);
