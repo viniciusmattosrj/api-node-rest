1c1cd7f00f5e77dedd3b6855e662147df28eeaaa
Vinicius Mattos <mvinicius@funcionalcorp.com.br>
2021-01-17 16:14:56 -0300

    17012021 - Adicionando configações dotenv.

---
+++ src/config/mail.js
@@ -0,0 +1,8 @@
+module.exports = {
+    mailHost:process.env.MAIL_HOST,
+    mailPort:process.env.MAIL_PORT,
+    mailUser:process.env.MAIL_USER,
+    mailPass:process.env.MAIL_PASS,
+    mailTo:process.env.MAIL_TO,
+    mailFrom:process.env.MAIL_FROM
+}
